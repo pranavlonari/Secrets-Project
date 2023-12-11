792a88a0ad1759001a7b9351ccd733b9191d0fc5
pranavlonari <pranavlonari12@gmail.com>
2023-12-11 13:08:54 +0530

    added code for password encryption

--- app.js
+++ app.js
@@ -1,63 +1,8 @@
-// //jshint esversion:6
-
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const ejs = require("ejs");
-// const mongoose = require("mongoose");
-
-// const app = express();
-
-// app.use(express.static("public"));
-// app.set("view engine", "ejs");
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// mongoose.connect("mongodb://localhost:27017/userDB", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
-// // Define the user schema
-// const userSchema = {
-//   email: String,
-//   password: String,
-// };
-
-// // Create the User model
-// const User = mongoose.model("User", userSchema);
-
-// app.get("/", function (req, res) {
-//   res.render("home");
-// });
-
-// app.get("/login", function (req, res) {
-//   res.render("login");
-// });
-
-// app.get("/register", function (req, res) {
-//   res.render("register");
-// });
-
-// app.post("/register", function (req, res) {
-//   const newUser = new User({
-//     email: req.body.username,
-//     password: req.body.password,
-//   });
-//   newUser.save(function (err) {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       res.render("secrets");
-//     }
-//   });
-// });
-
-// app.listen(3000, () => console.log(`Example app listening on port 3000!`));
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-
+const encrypt = require("mongoose-encryption");
 const app = express();
 
 app.use(express.static("public"));
@@ -65,10 +10,14 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect("mongodb://127.0.0.1:27017/userDB", { useNewUrlParser: true });
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-};
+});
+
+const secret = "Thisisourlittlesecret.";
+
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 app.get("/", function (req, res) {
